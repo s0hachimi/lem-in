e24b938f7386ebbd4da1adcb8510cb4c97bee8c5
Soufiane Hachimi <73444562+s0hachimi@users.noreply.github.com>
2024-11-23 17:23:37 +0100

    Add files via upload

--- main.go
+++ main.go
@@ -7,21 +7,61 @@ import (
 	"strings"
 )
 
-var Naml struct {
-	Nmala int
+var Ants struct {
+	count int
 	Start string
 	End   string
 	Rooms []string
-	Links []string
+	Links string
 }
 
 var Graph struct {
 	Path []string
 }
 
-
 func main() {
 	Readfile()
+	LinksFinder(Ants.Links)
+}
+
+func Paths() {
+}
+
+func LinksFinder(Links string) {
+	
+	
+
+	roomAndLinks := make(map[string][]string)
+
+	for i := 0; i <= len(Links)-1; i++ {
+		
+		if Links[i] == '*' {
+			continue
+		}
+
+		for j := 0; j <= len(Links)-1; j++ {
+			if Links[j] == '*' {
+				continue
+			}
+			if Links[i] == Links[j] {
+				if j != len(Links)-1 && Links[j+1] == '*' {
+					roomAndLinks[string(Links[i])] = append(roomAndLinks[string(Links[i])], string(Links[j-1]))
+				} else if j != len(Links)-1 {
+					roomAndLinks[string(Links[i])] = append(roomAndLinks[string(Links[i])], string(Links[j+1]))
+				}
+			}
+		}
+		
+	}
+
+	// slFilter := []string{}
+
+	// for _, l := range sl2 {
+	// 	for _, l2 := range sl2 {
+	// 	}
+	// }
+
+	fmt.Println(roomAndLinks)
 }
 
 func Readfile() {
@@ -37,21 +77,21 @@ func Readfile() {
 		fmt.Println(err)
 		return
 	}
-	Naml.Nmala = n
+	Ants.count = n
 
-	fmt.Printf("%#v\n", lines)
+	// fmt.Printf("%#v\n", lines)
 
 	for i := 0; i <= len(lines)-1; i++ {
 
-		if len(lines[i]) != 0 && string(lines[i][0]) == "#" && string(lines[i][1]) != "#" {
+		if lines[i] == "" || len(lines[i]) != 0 && string(lines[i][0]) == "#" && string(lines[i][1]) != "#" {
 			continue
 		}
 
 		if lines[i] == "##start" {
 			for j := 0; j <= len(lines[i+1])-1; j++ {
 				if lines[i+1][j] == ' ' {
-					Naml.Start = string(lines[i+1][:j])
-                    break
+					Ants.Start = string(lines[i+1][:j])
+					break
 				}
 			}
 		}
@@ -59,28 +99,37 @@ func Readfile() {
 		if lines[i] == "##end" {
 			for j := 0; j <= len(lines[i+1])-1; j++ {
 				if lines[i+1][j] == ' ' {
-					Naml.End = string(lines[i+1][:j])
-                    break
+					Ants.End = string(lines[i+1][:j])
+					break
 				}
 			}
 		}
 
 		if strings.Contains(lines[i], "-") {
-			Naml.Links = append(Naml.Links, lines[i])
+			lines[i] = strings.ReplaceAll(lines[i], "-", "")
+			if i != len(lines)-1 {
+				Ants.Links += lines[i] + "*"
+				// fmt.Println(len(lines)-1)
+				// fmt.Println(i)
+			} else {
+				Ants.Links += lines[i]
+			}
+
 		} else {
 			for j := 0; j <= len(lines[i])-1; j++ {
 				if lines[i][j] == ' ' {
-					Naml.Rooms = append(Naml.Rooms,  string(lines[i][:j]))
-                    break
+					Ants.Rooms = append(Ants.Rooms, string(lines[i][:j]))
+					break
 				}
 			}
 		}
 
 	}
-	// fmt.Println(Naml.Nmala)
-	// fmt.Println(Naml.Start)
-	// fmt.Println(Naml.End)
-    // fmt.Printf("%#v\n",Naml.Links)
-	// fmt.Printf("%#v\n",Naml.Rooms)
-	// fmt.Printf("%#v\n",Naml)
+
+	// fmt.Println(Ants.Nmala)
+	// fmt.Println(Ants.Start)
+	// fmt.Println(Ants.End)
+	fmt.Printf("%#v\n", Ants.Links)
+	// fmt.Printf("%#v\n",Ants.Rooms)
+	// fmt.Printf("%#v\n",Ants)
 }
